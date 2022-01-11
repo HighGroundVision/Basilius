f20bf8797db36e27c7867987d3d08224f0399a2c
RGBKnights <master@rgbknights.com>
2022-01-10 21:05:18 -0500

    removed the temp stuff from the build

--- TransferData.ps1
+++ TransferData.ps1
@@ -5,5 +5,5 @@ Copy-Item "$location\Abilities.json" -Destination "$location\src\Data\Abilities.
 Copy-Item "$location\Clusters.json" -Destination  "$location\src\Data\Clusters.json"
 Copy-Item "$location\\Heroes.json" -Destination "$location\src\Data\Heroes.json"
 Copy-Item "$location\Items.json" -Destination "$location\src\Data\Items.json"
-Copy-Item "$location\Modes.json" -Destination  "$location\src\Data\Modes.json"
-Copy-Item "$location\\Regions.json" -Destination  "$location\src\Data\Regions.json"
+#Copy-Item "$location\Modes.json" -Destination  "$location\src\Data\Modes.json"
+#Copy-Item "$location\\Regions.json" -Destination  "$location\src\Data\Regions.json"
