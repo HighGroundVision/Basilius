8fd8e227d8d21d0364af62eda7a39325b3281839
RGBKnights <master@rgbknights.com>
2022-01-10 21:25:12 -0500

    missed one

--- TransferData.ps1
+++ TransferData.ps1
@@ -2,8 +2,8 @@ $location = Get-Location
 get-childitem $location | foreach-object {write-host $_.FullName}
 
 Copy-Item "$location\Abilities.json" -Destination "$location\src\Data\Abilities.json"
-Copy-Item "$location\Clusters.json" -Destination  "$location\src\Data\Clusters.json"
 Copy-Item "$location\\Heroes.json" -Destination "$location\src\Data\Heroes.json"
 Copy-Item "$location\Items.json" -Destination "$location\src\Data\Items.json"
+#Copy-Item "$location\Clusters.json" -Destination  "$location\src\Data\Clusters.json"
 #Copy-Item "$location\Modes.json" -Destination  "$location\src\Data\Modes.json"
 #Copy-Item "$location\\Regions.json" -Destination  "$location\src\Data\Regions.json"
